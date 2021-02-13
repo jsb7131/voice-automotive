c8ec95950292bd3c616821eaac69b0645b66c06f
jsb7131 <jasonstephenbaker@gmail.com>
2021-02-12 21:36:22 -0500

    Cursor styling

--- src/components/VehiclesView.tsx
+++ src/components/VehiclesView.tsx
@@ -8,7 +8,7 @@ const VehiclesView: React.FC = () => {
     return (
         <>
             <button onClick={() => vehicles.add({make: "Alta", model: "Redshift"})}>Add Alta Redshift</button>
-            {vehicles.current.map(vehicle => <div key={vehicle.id} onClick={() => vehicles.remove(vehicle.id)}>{vehicle.make + " " + vehicle.model}</div>)}
+            {vehicles.current.map(vehicle => <div key={vehicle.id} style={{cursor: "pointer"}} onClick={() => vehicles.remove(vehicle.id)}>{vehicle.make + " " + vehicle.model}</div>)}
             <button onClick={vehicles.clear}>Clear</button>
             <button onClick={vehicles.reset}>Reset</button>
         </>
