ee565fd4988fb65967b7fc570e6213959b1fce12
jsb7131 <jasonstephenbaker@gmail.com>
2021-02-14 18:28:46 -0500

    Clear button conditional render

--- src/components/VehiclesView.tsx
+++ src/components/VehiclesView.tsx
@@ -9,7 +9,7 @@ const VehiclesView: React.FC = () => {
         <>
             <button onClick={() => vehicles.add({make: "Alta", model: "Redshift"})}>Add Alta Redshift</button>
             {vehicles.current.map(vehicle => <div key={vehicle.id} style={{cursor: "pointer"}} onClick={() => vehicles.remove(vehicle.id)}>{vehicle.make + " " + vehicle.model}</div>)}
-            <button onClick={vehicles.clear}>Clear</button>
+            {vehicles.current.length > 0 && <button onClick={vehicles.clear}>Clear</button>}
             <button onClick={vehicles.reset}>Reset</button>
         </>
     );
