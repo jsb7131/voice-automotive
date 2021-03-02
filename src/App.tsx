104105d073366eb4ba116b26717aba135681ecff
jsb7131 <jasonstephenbaker@gmail.com>
2021-03-01 23:44:46 -0500

    Created VehicleBlock component

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,16 @@ import { useLayout } from './hooks/LayoutContext';
 import { VehiclesProvider } from './hooks/VehiclesContext';
 import { Panel } from './components/Panel';
 import { VehiclesView } from './components/VehiclesView';
+import { VehicleBlock } from './components/VehicleBlock';
 
 export default function App() {
   const panel = useLayout();
   return (
     <div className="App">
         <header className="App-header">
-          <Panel isCollapsed={panel.collapsed} />
+          <Panel isCollapsed={panel.collapsed}>
+            <VehicleBlock></VehicleBlock>
+          </Panel>
           <button onClick={() => panel.collapse(panel.collapsed)}>{panel.collapsed ? "Open Panel" : "Close Panel"}</button>
           <img src={logo} className="App-logo" alt="logo" />
           <VehiclesProvider>
---
+++ src/components/VehicleBlock.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100px;
+    height: 100px;
+    background: white;
+    border-radius: 2px;
+    margin-top: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    &:hover {
+        background: green;
+    }
+`;
+
+export const VehicleBlock: React.FC = () => {
+    return (
+        <>
+            <Container></Container>
+        </>
+    );
+};
