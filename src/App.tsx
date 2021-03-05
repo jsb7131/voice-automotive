63a8c2f1efdb656141e47acb41f566f341d8c0a0
jsb7131 <jasonstephenbaker@gmail.com>
2021-03-05 18:23:59 -0500

    Added width prop to VehicleBlock

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ export default function App() {
     <div className="App">
         <header className="App-header">
           <Panel isCollapsed={panel.collapsed}>
-            <VehicleBlock BGColor={"red"} HoverColor={"green"} />
-            <VehicleBlock BGColor={"white"} HoverColor={"green"} />
-            <VehicleBlock BGColor={"blue"} HoverColor={"green"} />
+            <VehicleBlock width={"120"} BGColor={"red"} HoverColor={"green"} />
+            <VehicleBlock width={"100"} BGColor={"white"} HoverColor={"green"} />
+            <VehicleBlock width={"120"} BGColor={"blue"} HoverColor={"green"} />
           </Panel>
           <button onClick={() => panel.collapse(panel.collapsed)}>{panel.collapsed ? "Open Panel" : "Close Panel"}</button>
           <img src={logo} className="App-logo" alt="logo" />
--- src/components/VehicleBlock.tsx
+++ src/components/VehicleBlock.tsx
@@ -4,10 +4,11 @@ import styled from 'styled-components';
 type VBProps = {
     BGColor: string;
     HoverColor: string;
+    width: string;
 }
 
 const Container = styled.div<VBProps>`
-    width: 100px;
+    width: ${p => p.width}px;
     height: 100px;
     background: ${p => p.BGColor};
     border-radius: 2px;
@@ -19,8 +20,9 @@ const Container = styled.div<VBProps>`
     }
 `;
 
-export const VehicleBlock: React.FC<VBProps> = ({ BGColor, HoverColor }) =>
+export const VehicleBlock: React.FC<VBProps> = ({ width, BGColor, HoverColor }) =>
     <Container
+        width={width}
         BGColor={BGColor}
         HoverColor={HoverColor}
     />
