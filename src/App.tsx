47b6726679ac826b1c5622a7d573ae55abbf1a31
jsb7131 <jasonstephenbaker@gmail.com>
2021-03-10 13:11:45 -0500

    Removed PanelBlocks component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,30 @@ import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 import { useLayout } from './hooks/LayoutContext';
-import { VehiclesProvider } from './hooks/VehiclesContext';
+import { useDealer } from './hooks/useDealer';
 import { Panel } from './components/Panel';
+import { VehicleBlock } from './components/VehicleBlock';
+import { VehiclesProvider } from './hooks/VehiclesContext';
 import { VehiclesView } from './components/VehiclesView';
-import { PanelBlocks } from './components/PanelBlocks';
 
 export default function App() {
+
   const panel = useLayout();
+  const dealer = useDealer();
+
   return (
     <div className="App">
         <header className="App-header">
           <Panel isCollapsed={panel.collapsed}>
-            <PanelBlocks/>
+            {dealer.current.map(vehicle => 
+              <VehicleBlock
+                key={vehicle.id}
+                width={100}
+                height={100}
+                BGColor={"white"}
+                HoverColor={"green"}
+              />
+            )}
           </Panel>
           <button onClick={() => panel.collapse(panel.collapsed)}>{panel.collapsed ? "Open Panel" : "Close Panel"}</button>
           <img src={logo} className="App-logo" alt="logo" />
