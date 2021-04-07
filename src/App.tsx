5effb6e610c1fce3900100fdc52fb3c68e74bb81
jsb7131 <jasonstephenbaker@gmail.com>
2021-04-07 18:31:29 -0400

    Implemented VehicleStack

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ import { useLayout } from './hooks/LayoutContext';
 import { useDealer } from './hooks/useDealer';
 import { useVehicles } from './hooks/VehiclesContext';
 import { Panel } from './components/Panel';
-import { VehicleBlock } from './components/VehicleBlock';
 import { VehiclesView } from './components/VehiclesView';
+import { VehicleStack } from './components/VehicleStack';
 
 export default function App() {
 
   const panel = useLayout();
   const dealer = useDealer();
   const vehicles = useVehicles();
-  const pxSquare = panel.collapsed ? 100 : 150;
-  const blockFontSize = panel.collapsed ? "calc(5px + 2vmin)" : "calc(10px + 2vmin)";
   const panelButtonText = panel.collapsed ? "Open" : "Close";
 
   return (
@@ -28,17 +26,10 @@ export default function App() {
             </button>
             {!panel.collapsed && <h2 style={{color: "white"}}>Dealership</h2>}
             {dealer.map((vehicle, i) =>
-              <VehicleBlock
+              <VehicleStack
                 key={vehicle.id}
-                width={pxSquare}
-                height={pxSquare}
-                fontSize={blockFontSize}
-                bgColor={"white"}
-                border={"none"}
-                horiCenter
-                hoverColor={"green"}
+                collapsed={panel.collapsed}
                 last={i === dealer.length - 1 ? true : false}
-                click={() => vehicles.add({make: vehicle.make, model: vehicle.model})}
                 dealerMake={vehicle.make}
                 dealerModel={vehicle.model}
               />
