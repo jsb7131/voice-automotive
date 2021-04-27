575b5bbc2434fbff2fb5c3cec928339567378f38
jsb7131 <jasonstephenbaker@gmail.com>
2021-04-26 23:44:50 -0400

    Added selectElement function to useSelection

--- src/hooks/useSelection.tsx
+++ src/hooks/useSelection.tsx
@@ -3,15 +3,30 @@ import { useState } from 'react';
 type SelectableElem = {id: string, selected: boolean};
 
 export const useSelection = () => {
+
     const [selection, setSelection] = useState<SelectableElem[]>([]);
+
     const addElement = (id: string) => {
         setSelection([
             {id, selected: false},
             ...selection
         ]);
     };
+
+    const selectElement = (id: string) => {
+        const newSelection = selection.map(elem => {
+            if (elem.id === id) {
+                return {...elem, selected: true};
+            } else {
+                return {...elem, selected: false};
+            };
+        });
+        setSelection(newSelection);
+    };
+    
     return {
         selection,
-        addElement
+        addElement,
+        selectElement
     };
 };
